f27d4ab29deeab29979090b034640461689529c4
Omar Juice <ojameer1@gmail.com>
2019-04-04 20:52:45 -0400

    fix

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { IDeferred } from './Deferred';
-export declare type Cache = {
-    [key: string]: IDeferred;
-};
-export declare type key = string | number;
-export declare type BatchingFunction = (keys: any[]) => Promise<any[]>;
